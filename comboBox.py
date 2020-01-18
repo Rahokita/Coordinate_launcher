573ef9732618eb950b31f7492ce80157bf8b7de6
rahokita <rahokita7352@yahoo.co.jp>
2020-01-18 13:31:47 +0900

    プロトタイプ完成

--- comboBox.py
+++ comboBox.py
@@ -1,12 +1,38 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import shutil
+import os
+import os.path
 
 def button1_clicked():
     print('v1 = %s' % v1.get())
-    quit()
+    rep_folder = v1.get()
+    folder_exchange("coodinate","coodinate_bk",rep_folder)
 
 def cb_selected(event):
     print('v1 = %s' % v1.get())
+
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        folder_reset("coodinate","coodinate_bk")
+        root.destroy()
+
+def folder_exchange(org_folder,bk_folder,rep_folder):
+    #既存のフォルダをremaneして退避
+    os.rename(org_folder, bk_folder)
+    #コピー先のフォルダを作成（フォルダ名は固定）
+    #os.mkdir(org_folder)
+    #フォルダの中身ごとコピー
+    shutil.copytree('Foo', org_folder)
+
+def folder_reset(org_folder,bk_folder):
+    #bk_dolderが存在するかをチェック
+    if os.path.exists(bk_folder) == True:
+        #フォルダを一旦削除
+        shutil.rmtree(org_folder)
+        #退避したフォルダを元に戻す
+        os.rename(bk_folder, org_folder)
 
 if __name__ == '__main__':
     root = Tk()
@@ -29,4 +55,5 @@ if __name__ == '__main__':
     button1 = ttk.Button(frame1, text='OK', command=button1_clicked)
     button1.grid(row=0, column=1)
     
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
